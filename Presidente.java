2f8f39a2877edf517ada3ffe348db3c9d3ebf3da
Gabriel <gabrielpauleti@outlook.com>
2024-01-22 20:33:02 -0300

    Initial commit of refactoring of project to reflect OOP in Java

---
+++ Presidente.java
@@ -0,0 +1,13 @@
+package UrnaOOP;
+public class Presidente {
+    public String nome;
+    public String partido;
+    public int numero;
+    public int Votos;
+
+public Presidente(String nome, String partido, int numero){
+    this.nome = nome;
+    this.partido = partido;
+    this.numero = numero;
+}
+}
